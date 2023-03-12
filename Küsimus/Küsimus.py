2374dfe19c94a0f3593be0b105ebd9db959d080c
Michael Odinokov <mihhail.odinokov@gmail.com>
2023-03-12 17:21:25 +0200

    12

--- Küsimus/Küsimus.py
+++ Küsimus/Küsimus.py
@@ -6,11 +6,9 @@ while True:
     kogus=int(input("Mitu inimesi osaleb?"))
     if kogus<=3:
         if kogus==1:
-            n=kusimus_vastused_1(input("Sissetage sinu nimi:"))
+            n=kusimus_vastused_1(input("Sissetage oma nimi:"))
         elif kogus==2:
-            for i in range(2):
-                n=kusimus_vastused_2(input("Sissetage sinu nimi:"))
-        elif kogus==3:
-            n=kusimus_vastused_3(input("Sissetage sinu nimi:"))
+            n=kusimus_vastused_2(input("Sissetage oma nimi:"))
+
 
     
--- Küsimus/Omamodul.py
+++ Küsimus/Omamodul.py
@@ -1,11 +1,8 @@
-﻿kus_vas={"Mis месяцы относятся к лету?":"juuni, juuli, august", "Какое растение символизирует лето?":"подсолнух", "Как называется праздник начала лета?":"летнее солнцестояние", "Как называется игра, которая играют на улице летом?":"лазертаг", "Какой фрукт считается символом лета?":"арбуз", "Какой цвет ассоциируется с летом?":"желтый", "Какой город в России считается летней столицей?":"Санкт-Петербург"}
-
-
+﻿kus_vas={"Mis месяцы относятся к лету?":"juuni, juuli, august", "Какое растение символизирует лето?":"подсолнух", "Как называется праздник начала лета?":"летнее солнцестояние", "Как называется игра, которая играют на улице летом?":"лазертаг", "Какой фрукт считается символом лета?":"арбуз"}
 
 def kusimus_vastused_1(n):
     """
-    """
-    
+    """    
     print(f"Tere, {n}! Alustame küsitlust.")
     punktid=0    
     for kusimus, vastus in kus_vas.items():
@@ -16,56 +13,32 @@ def kusimus_vastused_1(n):
         else:
             print("Vale vastus!")    
     if punktid>3:
-        print(f"Tubli töö, {n}! Teie skoor oli {punktid}/7.")
+        print(f"Tubli töö, {n}! Teie skoor oli {punktid}/5.")
         with open("oiged.txt", "a", encoding="utf-8") as f:
-            f.write(f"{n}: {punktid}/7\n")
+            f.write(f"{n}: {punktid}/5\n")
+    else:
+        print(f"Pole paha, {n}. Teie skoor oli {punktid}/5.")
+        with open("valed.txt", "a", encoding="utf-8") as f:
+            f.write(f"{n}: {punktid}/5\n")
+"""
+def kusimus_vastused_2(n):
+    """
+    """          
+    print(f"Tere, {n}! Alustame küsitlust.")
+    punktid=0    
+    for kusimus, vastus in kus_vas.items():
+        kasutaja_vastus=input(kusimus)
+        if kasutaja_vastus.lower()==vastus.lower():
+            print("Õige vastus!")
+            punktid+=1
+        else:
+            print("Vale vastus!")    
+    if punktid > 3:
+        print(f"Tubli töö, {n}! Teie skoor oli {punktid}/5.")
+        with open("oiged.txt", "a", encoding="utf-8") as f:
+            f.write(f"{n}: {punktid}/5\n")
     else:
         print(f"Pole paha, {n}. Teie skoor oli {punktid}/7.")
         with open("valed.txt", "a", encoding="utf-8") as f:
-            f.write(f"{n}: {punktid}/7\n")
-
-def kusimus_vastused_2():
-    """
-    """
-    for i in range(2):
-        n = input("Sisestage oma nimi: ")
-        print(f"Tere, {n}! Alustame küsitlust.")
-        punktid = 0    
-        for kusimus, vastus in kus_vas.items():
-            kasutaja_vastus = input(kusimus)
-            if kasutaja_vastus.lower() == vastus.lower():
-                print("Õige vastus!")
-                punktid += 1
-            else:
-                print("Vale vastus!")    
-        if punktid > 3:
-            print(f"Tubli töö, {n}! Teie skoor oli {punktid}/7.")
-            with open("oiged.txt", "a", encoding="utf-8") as f:
-                f.write(f"{n}: {punktid}/7\n")
-        else:
-            print(f"Pole paha, {n}. Teie skoor oli {punktid}/7.")
-            with open("valed.txt", "a", encoding="utf-8") as f:
-                f.write(f"{n}: {punktid}/7\n")
-
-def kusimus_vastused_3():
-    """
-    """
-    for i in range(3):
-        n = input("Sisestage oma nimi: ")
-        print(f"Tere, {n}! Alustame küsitlust.")
-        punktid = 0    
-        for kusimus, vastus in kus_vas.items():
-            kasutaja_vastus = input(kusimus)
-            if kasutaja_vastus.lower() == vastus.lower():
-                print("Õige vastus!")
-                punktid += 1
-            else:
-                print("Vale vastus!")    
-        if punktid > 3:
-            print(f"Tubli töö, {n}! Teie skoor oli {punktid}/7.")
-            with open("oiged.txt", "a", encoding="utf-8") as f:
-                f.write(f"{n}: {punktid}/7\n")
-        else:
-            print(f"Pole paha, {n}. Teie skoor oli {punktid}/7.")
-            with open("valed.txt", "a", encoding="utf-8") as f:
-                f.write(f"{n}: {punktid}/7\n")
+            f.write(f"{n}: {punktid}/5\n")
+"""
